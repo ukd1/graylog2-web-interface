9f642cf67836e8a2a6f0f1644bd50012be31c2eb
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2011-04-28 23:26:41 +0400

    Aggregate stats of alive servers only. #WEBINTERFACE-45

--- app/models/server_value.rb
+++ app/models/server_value.rb
@@ -26,16 +26,20 @@ class ServerValue
     updated_at > 65.seconds.ago
   end
 
+  def self.all_alive
+    (all || []).select(&:alive?)
+  end
+
   def self.all_alive?
-    all.map(&:alive?).reduce(&:&)
+    all_alive.count > 0
   end
 
   def self.total_current_messages_throughput
-    aggregate(:messages_throughput, 'current')
+    aggregate_alive(:messages_throughput, 'current')
   end
 
   def self.total_highest_messages_throughput
-    aggregate(:messages_throughput, 'highest')
+    aggregate_alive(:messages_throughput, 'highest')
   end
 
   def self.remove_dead_servers
@@ -43,7 +47,7 @@ class ServerValue
   end
 
 private
-  def self.aggregate(field, key)
-    all.map(&field).map { |hash| hash.fetch(key, nil) }.compact.reduce(&:+)
+  def self.aggregate_alive(field, key)
+    all_alive.map(&field).map { |hash| hash.fetch(key, nil) }.compact.reduce(&:+) || 0
   end
 end
--- test/unit/server_value_test.rb
+++ test/unit/server_value_test.rb
@@ -1,12 +1,15 @@
 require 'test_helper'
 
 class ServerValueTest < ActiveSupport::TestCase
-  should "calculate total throughput" do
-    ServerValue.make(:messages_throughput => {'current' => 7})
-    ServerValue.make(:messages_throughput => {'current' => 8})
-    ServerValue.make(:messages_throughput => {'aaaaaaa' => 9})
-    ServerValue.make(:messages_throughput => nil)
+  should "calculate total throughput of alive servers" do
+    assert_equal 0, ServerValue.total_current_messages_throughput
 
-    assert_equal 15, ServerValue.total_current_messages_throughput
+    ServerValue.make(:messages_throughput => {'current' => 1})
+    ServerValue.make(:messages_throughput => {'current' => 10})
+    ServerValue.make(:messages_throughput => {'aaaaaaa' => 100})
+    ServerValue.make(:messages_throughput => nil)
+    ServerValue.make(:messages_throughput => {'current' => 1000}, :updated_at => 70.seconds.ago)
+
+    assert_equal 11, ServerValue.total_current_messages_throughput
   end
 end
